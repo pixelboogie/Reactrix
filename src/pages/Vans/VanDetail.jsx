4bea291dab142d8fa8042c9d6cb2651a24408cd8
pixelboogie <justamovie@yahoo.com>
2024-09-30 12:28:18 -0500

    conditionally render the back button text

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -15,6 +15,7 @@ export default function VanDetail() {
     },[params.id])
 
     const search = location.state?.search || ""
+    const type = location.state?.type || "all"
     
     return (
         <div className="van-detail-container">
@@ -22,7 +23,7 @@ export default function VanDetail() {
                 to={`..${location.state.search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
