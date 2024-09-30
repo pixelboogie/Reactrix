8f94ff871a906a93ed8f0bb7dce3ad5ea2dc63ad
pixelboogie <justamovie@yahoo.com>
2024-09-30 07:48:08 -0500

    Relative Links, Back to All Vans, Add /host/vans/:id Nested Routes

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,9 @@ import Layout from "../components/Layout"
 import Dashboard from "./pages/Host/Dashboard"
 import HostVans from "./pages/Host/HostVans"
 import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import HostLayout from "../components/HostLayout"
@@ -29,11 +32,15 @@ function App() {
           <Route path="vans/:id" element={<VanDetail />}/>
 
           <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />}/>
-            <Route path="income" element={<Income />}/>
-            <Route path="reviews" element={<Reviews />}/>
-            <Route path="vans" element={<HostVans />}/>
-            <Route path="vans/:id" element={<HostVanDetail />}/>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
