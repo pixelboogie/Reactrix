ff068c6a4519a677114b535c7940116924c66009
pixelboogie <justamovie@yahoo.com>
2024-09-30 11:07:08 -0500

    Merging search params with the setSearchParams function

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -33,14 +33,29 @@ export default function Vans() {
         </div>
     ))
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams("?type=simple")} className="van-type simple">Simple</button>
-                <button onClick={() => setSearchParams("?type=luxury")} className="van-type luxury">Luxury</button>
-                <button onClick={() => setSearchParams("?type=rugged")} className="van-type rugged">Rugged</button>
-                <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear Filter</button>
+                <button onClick={() => handleFilterChange("type", "simple")} className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}>Simple</button>
+                <button onClick={() => handleFilterChange("type", "luxury")} className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`}>Luxury</button>
+                <button onClick={() => handleFilterChange("type", "rugged")} className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}>Rugged</button>
+                {
+                    typeFilter ? (
+                        <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear Filter</button>
+                    ) : null
+                }
             
             </div>
             <div className="van-list">
